07eeddb85a3cf9e3766c311f37c673b6a3ea6275
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2022-10-24 22:24:53 -0300

    atualização

--- index.php
+++ index.php
@@ -1,27 +1,8 @@
 <?php
-    $artigo1= [
-        ['titulo' => 'Primeiros passos com Spring',
-        'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-        programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-        'link' => 'primeiros-passos-com-spring.html'
-    ],
-        [
-            'titulo' => 'O que é Metodologia Ágil?',
-            'conteudo' => 'Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
-            'link' => 'o-que-e-metodologia-agil.html'
-        ],  
-        [
-            'titulo' => 'Como é o funil do Growth Hacking?',
-            'conteudo' => 'Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-            mais sobre ele.',
-            'link' => 'como-e-o-funil-do-growth-hacking.html'
-        ],
-        [
-            'titulo' => 'Bom Dia',
-            'conteudo' => 'jogo',
-            'link' => 'oi turu bem? quero comer'
-        ]
-    ];
+    require 'config.php';
+    include 'artigos.php';
+    $artigo1 = new artigo1();
+    $artigo1 =  
 ?>
 
 <!DOCTYPE html>
